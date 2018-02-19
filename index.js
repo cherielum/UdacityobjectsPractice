f7241fe2d92a5601a336f668404bb0adaae26306
Cherie Lum <charismatic.cherie@gmail.com>
2018-02-19 17:38:43 -0500

    adding exercise

--- index.js
+++ index.js
@@ -116,9 +116,23 @@ var facebookProfile = {
     
     removeFriend: function() {
         facebookProfile.friends -=1;
-    }
-    
-    
+    }    
 };
+
+//Use the forEach() method to loop over the array and 
+//print out the following donut summaries using console.log.
+
+var donuts = [
+    { type: "Jelly", cost: 1.22 },
+    { type: "Chocolate", cost: 2.45 },
+    { type: "Cider", cost: 1.59 },
+    { type: "Boston Cream", cost: 5.99 }
+];
+
+// your code goes here
+donuts.forEach(function(donuts) {
+    console.log(donuts.type +" donuts cost $" + donuts.cost + " each");
+});
+
 
 
